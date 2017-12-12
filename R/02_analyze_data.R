486cc729db8b0a53ca69bdfd1b5b15f120d57c4c
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-12-12 16:43:15 -0500

    Explain analysis function

--- R/02_analyze_data.R
+++ R/02_analyze_data.R
@@ -7,46 +7,62 @@
 ################################################################################
 
 ###   coxph analysis function (unadj, iptw, mw, ow) for anti-diabetics data
+##' Analyze outcome using glm
+##'
+##' .. content for details ..
+##'
+##' @param data data_frame assumed to be trimmed already.
+##' @param formula
+##' @param family
+##'
+##' @return data_frame with an adjustment method column and a model fit list column.
+##'
+##' @export
 analyze_outcome_glm <- function(data, formula, family) {
+
+    ## model = FALSE
+    ## a logical value indicating whether _model frame_ should be kept.
+    ## Drop the model frame to save space.
 
     lst <- list(unadj = try(glm(formula = formula,
                                 family = family,
                                 data = data,
-                                subset = (keep == 1))),
+                                model = FALSE)),
                 iptw1 = try(glm(formula = formula,
                                 family = family,
                                 data = data,
                                 weights = iptw1,
-                                subset = (keep == 1),
-                                robust = TRUE)),
+                                robust = TRUE,
+                                model = FALSE)),
                 iptw2 = try(glm(formula = formula,
                                 family = family,
                                 data = data,
                                 weights = iptw2,
-                                subset = (keep == 1))),
+                                model = FALSE)),
                 mw1 = try(glm(formula = formula,
                               family = family,
                               data = data,
                               weights = mw1,
-                              subset = (keep == 1))),
+                              model = FALSE)),
                 mw2 = try(glm(formula = formula,
                               family = family,
                               data = data,
                               weights = mw2,
-                              subset = (keep == 1))),
+                              model = FALSE)),
                 ow1 = try(glm(formula = formula,
                               family = family,
                               data = data,
                               weights = ow1,
-                              subset = (keep == 1))),
+                              model = FALSE)),
                 ow2 = try(glm(formula = formula,
                               family = family,
                               data = data,
                               weights = ow2,
-                              subset = (keep == 1)))
+                              model = FALSE))
                 )
 
     data_frame(adjustment = names(lst),
+               ## List column
                model = lst)
 }
 
